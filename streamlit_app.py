d5f84e724b121819edbfdff68b6ac6eda21860d9
BoboBink <40386026+BoboBink@users.noreply.github.com>
2022-04-24 01:04:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard boiled Free-Range Eggs')
